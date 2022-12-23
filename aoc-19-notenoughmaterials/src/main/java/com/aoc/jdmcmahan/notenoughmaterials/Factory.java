11e57909ccc8716afb0789e15ab0db19fd57ba7a
jdmcmahan <jdmcmahan@gmail.com>
2022-12-23 15:28:44 -0600

    Fixes to get tests passing (finally)

--- aoc-19-notenoughmaterials/src/main/java/com/aoc/jdmcmahan/notenoughmaterials/Factory.java
+++ aoc-19-notenoughmaterials/src/main/java/com/aoc/jdmcmahan/notenoughmaterials/Factory.java
@@ -11,69 +11,147 @@ import lombok.Getter;
 
 import java.util.*;
 
+@Getter
 public class Factory {
 
-    @Getter
+    private final Factory parent;
+
     private final BlueprintSet blueprints;
 
-    @Getter
     private final ResourceStockpile resources;
 
     private final List<Robot> robots = new LinkedList<>();
-    private final Robot pending;
+    private Robot pending;
 
-    public Factory(BlueprintSet blueprints) {
+    private final int maxTicks;
+    private int currentTick;
+
+    public Factory(BlueprintSet blueprints, int maxTicks) {
+        this.parent = null;
         this.blueprints = blueprints;
         this.resources = new ResourceStockpile();
 
         this.robots.add(new OreRobot());
         this.pending = null;
+
+        this.maxTicks = maxTicks;
+        this.currentTick = 0;
     }
 
-    protected Factory(Factory other, RobotBlueprint<?> toBuild) {
-        this.blueprints = other.blueprints;
-        this.resources = new ResourceStockpile(other.resources);
+    protected Factory(Factory parent, RobotBlueprint<?> toBuild) {
+        this.parent = parent;
+        this.blueprints = parent.blueprints;
+        this.resources = new ResourceStockpile(parent.resources);
 
-        this.robots.addAll(other.robots);
+        this.robots.addAll(parent.robots);
 
         this.pending = toBuild != null
                 ? toBuild.build(this.resources)
                 : null;
+
+        this.maxTicks = parent.maxTicks;
+        this.currentTick = parent.currentTick;
     }
 
-    public Factory minmax(Comparator<Factory> comparator, int remaining) {
+    public int getRemainingTicks() {
+        return this.maxTicks - this.currentTick;
+    }
+
+    public List<Factory> path() {
+        LinkedList<Factory> path = new LinkedList<>();
+        Factory factory = this;
+
+        do {
+            path.addFirst(factory);
+        } while ((factory = factory.getParent()) != null);
+
+        return path;
+    }
+
+    public Factory minmax() {
+        return this.minmax(0);
+    }
+
+    protected Factory minmax(int bestScore) {
+        this.tick();
+
+        if (this.getRemainingTicks() == 0) {
+            return this;
+        }
+
+        // If we can't possibly beat the best score, stop now
+        if (this.optimisticBest(Resource.GEODE) < bestScore) {
+            return this;
+        }
+
+        Factory best = this;
+        for (Factory future : this.futures()) {
+            future = future.minmax(bestScore);
+
+            int score = future.resources.getStock(Resource.GEODE);
+            if (score > bestScore) {
+                best = future;
+                bestScore = score;
+            }
+        }
+
+        return best;
+    }
+
+    protected void tick() {
+        if (this.getRemainingTicks() <= 0) {
+            throw new IllegalArgumentException("No more ticks!");
+        }
+
         robots.stream()
                 .map(Robot::output)
                 .forEach(resources::credit);
 
         if (pending != null) {
             robots.add(pending);
+            pending = null;
         }
 
-        if (remaining == 0) {
-            return this;
-        }
-
-        return this.futures().stream()
-                .map(future -> future.minmax(comparator, remaining - 1))
-                .reduce(this, (a, b) -> comparator.compare(a, b) < 0 ? a : b);
+        this.currentTick++;
     }
 
-    public Collection<Factory> futures() {
-        List<Factory> futures = new LinkedList<>();
+    protected Collection<Factory> futures() {
+        int remainingTicks = this.getRemainingTicks();
 
-        for (Resource resource : Resource.values()) {
-            if (resource == Resource.GEODE || resources.getStock(resource) + this.currentProduction(resource) <= maxSpending(resource)) {
+        if (remainingTicks <= 0) {
+            return Collections.emptyList();
+        } else if (remainingTicks == 1) {
+            // No sense in building anything if there's not enough time left to affect production
+            return Collections.singletonList(new Factory(this, null));
+        }
+
+        // Always build a geode robot if we can
+        RobotBlueprint<?> geodeBlueprint = blueprints.getBlueprint(Resource.GEODE);
+        if (geodeBlueprint.canAfford(resources)) {
+            return Collections.singletonList(new Factory(this, geodeBlueprint));
+        }
+
+        List<Factory> futures = new LinkedList<>();
+        for (Resource resource : Arrays.asList(Resource.OBSIDIAN, Resource.CLAY, Resource.ORE)) {
+
+            // Only build this robot if we could use more output
+            if (this.currentProduction(resource) < maxSpending(resource)) {
                 RobotBlueprint<?> blueprint = blueprints.getBlueprint(resource);
+
+                // If we skipped this blueprint last time and didn't build something else instead, prune this branch
+                if (parent != null && blueprint.canAfford(parent.resources) && parent.robots.size() == this.robots.size()) {
+                    continue;
+                }
+
                 if (blueprint.canAfford(resources)) {
                     futures.add(new Factory(this, blueprint));
                 }
             }
         }
 
-        if (futures.isEmpty()) {
-            futures.add(new Factory(this, null));
-        }
+        // Non-ideal case - do nothing while we wait for more resources
+        // This is added last so that it can be pruned more readily
+        futures.add(new Factory(this, null));
 
         return futures;
     }
@@ -89,6 +167,30 @@ public class Factory {
     protected int maxSpending(Resource resource) {
         return blueprints.stream()
                 .mapToInt(blueprint -> blueprint.cost(resource))
-                .sum();
+                .max()
+                .orElse(0);
+    }
+
+    protected int optimisticBest(Resource resource) {
+        int remaining = this.getRemainingTicks();
+        int total = 0;
+
+        // Current stock...
+        total += this.resources.getStock(resource);
+
+        // ... plus production from existing robots...
+        total += remaining * robots.stream()
+                .filter(robot -> robot.output().resource() == resource)
+                .count();
+
+        // ... plus future production from the robot currently being built (if applicable)...
+        total += (pending != null && pending.output().resource() == resource)
+                ? remaining - 1
+                : 0;
+
+        // ... plus future production under the (rather optimistic) assumption that we can build another robot every turn
+        total += (remaining * (remaining - 1)) / 2;
+
+        return total;
     }
 }
--- aoc-19-notenoughmaterials/src/test/java/com/aoc/jdmcmahan/notenoughmaterials/FactoryMinMaxTest.java
+++ aoc-19-notenoughmaterials/src/test/java/com/aoc/jdmcmahan/notenoughmaterials/FactoryMinMaxTest.java
@@ -2,15 +2,17 @@ package com.aoc.jdmcmahan.notenoughmaterials;
 
 import com.aoc.jdmcmahan.notenoughmaterials.model.BlueprintSet;
 import com.aoc.jdmcmahan.notenoughmaterials.model.Resource;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class FactoryMinMaxTest {
 
@@ -20,16 +22,26 @@ class FactoryMinMaxTest {
             BlueprintParser parser = new BlueprintParser();
             List<BlueprintSet> blueprintSets = parser.parse(input);
 
-            Comparator<Factory> comparator = Comparator.comparingInt(factory -> factory.getResources().getStock(Resource.GEODE));
+            Map<Integer, Factory> minmax = blueprintSets.stream()
+                    .map(blueprints -> new Factory(blueprints, 24))
+                    .map(Factory::minmax)
+                    .collect(Collectors.toMap(factory -> factory.getBlueprints().getId(), Function.identity()));
 
-            Factory optimal = blueprintSets.stream()
-                    .map(Factory::new)
-                    .map(factory -> factory.minmax(comparator.reversed(), 24))
-                    .max(comparator)
-                    .orElseGet(Assertions::fail);
+            assertEquals(2, minmax.size());
 
-            assertEquals(2, optimal.getBlueprints().getId());
-            assertEquals(12, optimal.getResources().getStock(Resource.GEODE));
+            Factory f1 = minmax.get(1);
+
+            assertNotNull(f1);
+            assertEquals(1, f1.getBlueprints().getId());
+            assertEquals(9, f1.getResources().getStock(Resource.GEODE));
+            assertEquals(9, this.qualityLevel(f1));
+
+            Factory f2 = minmax.get(2);
+
+            assertNotNull(f2);
+            assertEquals(2, f2.getBlueprints().getId());
+            assertEquals(12, f2.getResources().getStock(Resource.GEODE));
+            assertEquals(24, this.qualityLevel(f2));
         }
     }
 
@@ -39,16 +51,58 @@ class FactoryMinMaxTest {
             BlueprintParser parser = new BlueprintParser();
             List<BlueprintSet> blueprintSets = parser.parse(input);
 
-            Comparator<Factory> comparator = Comparator.<Factory>comparingInt(factory -> factory.getResources().getStock(Resource.GEODE))
-                    .reversed();
-
             int qualityLevel = blueprintSets.stream()
-                    .map(Factory::new)
-                    .map(factory -> factory.minmax(comparator, 24))
+                    .map(blueprints -> new Factory(blueprints, 24))
+                    .map(Factory::minmax)
                     .mapToInt(this::qualityLevel)
                     .sum();
 
-            assertEquals(12, qualityLevel);
+            assertEquals(1675, qualityLevel);
+        }
+    }
+
+    @Test
+    void testMinMaxPostElephantSatiety_withExampleInput() throws IOException {
+        try (InputStream input = FactoryMinMaxTest.class.getClassLoader().getResourceAsStream("example.txt")) {
+            BlueprintParser parser = new BlueprintParser();
+            List<BlueprintSet> blueprintSets = parser.parse(input);
+
+            Map<Integer, Factory> minmax = blueprintSets.stream()
+                    .map(blueprints -> new Factory(blueprints, 32))
+                    .map(Factory::minmax)
+                    .collect(Collectors.toMap(factory -> factory.getBlueprints().getId(), Function.identity()));
+
+            assertEquals(2, minmax.size());
+
+            Factory f1 = minmax.get(1);
+
+            assertNotNull(f1);
+            assertEquals(1, f1.getBlueprints().getId());
+            assertEquals(56, f1.getResources().getStock(Resource.GEODE));
+
+            Factory f2 = minmax.get(2);
+
+            assertNotNull(f2);
+            assertEquals(2, f2.getBlueprints().getId());
+            assertEquals(62, f2.getResources().getStock(Resource.GEODE));
+        }
+    }
+
+    @Test
+    void testMinMaxPostElephantSatiety_withChallengeInput() throws IOException {
+        try (InputStream input = FactoryMinMaxTest.class.getClassLoader().getResourceAsStream("challenge.txt")) {
+            BlueprintParser parser = new BlueprintParser();
+            List<BlueprintSet> blueprintSets = parser.parse(input);
+
+            blueprintSets = blueprintSets.subList(0, 3);
+
+            int qualityLevel = blueprintSets.stream()
+                    .map(blueprints -> new Factory(blueprints, 32))
+                    .map(Factory::minmax)
+                    .mapToInt(factory -> factory.getResources().getStock(Resource.GEODE))
+                    .reduce(1, (l, r) -> l * r);
+
+            assertEquals(6840, qualityLevel);
         }
     }
 
